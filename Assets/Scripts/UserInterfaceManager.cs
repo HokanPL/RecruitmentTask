3789aafc801af77f0af9dc690c651a155233b9c3
Michał Mulewicz <michal.mulewicz@gmail.com>
2020-07-13 18:56:28 +0200

    Initial commit.

---
+++ Assets/Scripts/UserInterfaceManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInterfaceManager : MonoBehaviour
+{
+    public Button firstButton;
+    public Button secondButton;
+    public Button thirdButton;
+
+    public Button closePanel;
+
+    public Toggle[] audioToggles;
+}
